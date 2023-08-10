8f06de406b8fa86677893d7e9ff3ab53f1b1ac6f
Freddy Morales <freddy.morales@usil.pe>
2023-08-09 22:12:15 -0500

    billboards

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,19 @@
-const HomePage = () => {
-  return (
-    <div>
-      Home page
-    </div>
-  )
-}
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
 
-export default HomePage
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("d8d21a61-d33e-411f-8e98-dd8e98fae97c");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
