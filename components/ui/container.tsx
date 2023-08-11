e5e1e1d0d82a91659662cdc5ec6dafff0988a2a1
Freddy Morales <freddy.morales@usil.pe>
2023-08-11 00:38:55 -0500

    gallery

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -8,7 +8,7 @@ const Container: React.FC<ContainerProps> = ({
   children,
 }) => {
   return (
-    <div className='mx-auto max-w-7xl'>
+    <div className='mx-auto max-w-7xl h-full'>
       {children}
     </div>
   )
