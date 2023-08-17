4b5a6f8b02b7f92f6dc6f42773535fd52cb8c9a8
Freddy Morales <freddy.morales@usil.pe>
2023-08-17 10:51:28 -0500

    preview product card

---
+++ providers/modal-provider.tsx
@@ -0,0 +1,21 @@
+"use client"
+import PreviewModal from '@/components/preview-modal'
+import React from 'react'
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) return null
+
+  return (
+    <>
+      <PreviewModal /> 
+    </>
+  )
+}
+
+export default ModalProvider
